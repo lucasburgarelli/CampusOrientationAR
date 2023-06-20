34863ce2961bf08eb3b410823257687602ebe29d
lucasburgarelli <lucasburgarellim@gmail.com>
2023-06-20 17:09:51 -0300

    MarkerDataSet done

---
+++ Assets/Markers/MarkerDataSet.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MarkerDataSet : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI txtClassroom, txtDatetimestart, txtDatetimeend, txtCoursename, txtDescription, txtNameteacher;
+    private void Start()
+    {
+        var dataGetter = new DataGetter();
+        CompleteClass completeClass = dataGetter.GetCompleteByClassroom(gameObject.name);
+
+        txtClassroom.text = completeClass.Classroom;
+        txtCoursename.text = completeClass.Coursename;
+        txtNameteacher.text = completeClass.Nameteacher;
+        txtDescription.text = completeClass.Description;
+        txtDatetimestart.text = completeClass.Datetimestart.ToLongTimeString();
+        txtDatetimeend.text = completeClass.Datetimeend.ToLongTimeString();
+
+    }
+}
